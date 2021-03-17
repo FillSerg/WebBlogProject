0ec2dc31197cd6eeb491685aeaeb49dce0a6022d
sfill70 <sergfill70@yandex.ru>
2021-03-17 18:34:42 +0300

    Stage 1 full

---
+++ src/main/java/main/controllers/ApiGeneralController.java
@@ -0,0 +1,57 @@
+package main.controllers;
+
+/*import main.api.response.InitResponse;
+import main.api.response.SettingsResponse;
+import main.api.response.TagResponse;
+import main.service.SettingsService;
+import main.service.TagService;*/
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RestController
+@RequestMapping(value = "/api")
+public class ApiGeneralController {
+
+    @GetMapping("")
+    ResponseEntity<String> getAuth() {
+        return new ResponseEntity<>("/api/", HttpStatus.OK);
+    }
+
+   /* private final InitResponse initResponse;
+    private final SettingsService settingsService;
+    private final TagService tagService;*/
+
+/*    public ApiGeneralController(InitResponse initResponse, SettingsService settingsService, TagService tagService) {
+        this.initResponse = initResponse;
+        this.settingsService = settingsService;
+        this.tagService = tagService;
+    }
+
+    @GetMapping("")
+    ResponseEntity<String> getApi() {
+        return new ResponseEntity<>("/api/", HttpStatus.OK);
+    }
+
+    @GetMapping("/init")
+    public InitResponse init() {
+        return initResponse;
+    }
+
+    *//*  @GetMapping("api/settings")
+      public SettingsResponse settings() {
+          return settingsService.getGlobalSettings();
+      }*//*
+    @GetMapping("/settings")
+    public ResponseEntity<SettingsResponse> settings() {
+        return ResponseEntity.ok(settingsService.getGlobalSettings());
+    }
+
+    @GetMapping("/tag")
+    public ResponseEntity<TagResponse> getTags(@RequestParam(name = "query", required = false) String query) {
+        return tagService.tag(query);
+    }*/
+}
---
+++ src/main/java/main/controllers/ApiPostController.java
@@ -0,0 +1,26 @@
+package main.controllers;
+
+//import main.api.response.PostResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequestMapping(value = "/api/post")
+public class ApiPostController {
+
+    @GetMapping("")
+    ResponseEntity<String> getAuth() {
+        return new ResponseEntity<>("/api/auth/", HttpStatus.OK);
+    }
+
+    /*@GetMapping("")
+    ResponseEntity<PostResponse> getPost() {
+        return ResponseEntity.ok(new PostResponse());
+    }*/
+
+}
